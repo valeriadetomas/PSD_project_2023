e56c1f4ffd71a113f6e9663a02e0df1dcd2bc4dd
valeria <valeria.detomas@mail.polimi.it>
2023-09-10 18:18:43 +0200

    added implementation

--- implementation/worker.js
+++ implementation/worker.js
@@ -1,5 +1,9 @@
-const { Client, logger } = require('camunda-external-task-client-js');
-const open = require('open');
+import { Client, logger, Variables} from 'camunda-external-task-client-js';
+import open from 'open';
+import request from 'request';
+
+//const { Client, logger } = require('camunda-external-task-client-js');
+//const open = require('open');
 
 // configuration for the Client:
 //  - 'baseUrl': url to the Process Engine
@@ -10,18 +14,143 @@ const config = { baseUrl: 'http://localhost:8080/engine-rest', use: logger, asyn
 // create a Client instance with custom configuration
 const client = new Client(config);
 
-// susbscribe to the topic: 'charge-card'
-client.subscribe('charge-card', async function({ task, taskService }) {
-  // Put your business logic here
+// susbscribe to the topic: 'send_req_quot_plumber'
+client.subscribe('send_req_quot_plumber', async function({ task, taskService }) {
 
-  // Get a process variable
-  const amount = task.variables.get('amount');
-  const item = task.variables.get('item');
+  const receiver = task.variables.get("p");
+  const description = task.variables.get("plumber_descr")
+  const budget = task.variables.get("plumber_budget");
 
-  console.log(`Charging credit card with an amount of ${amount}€ for the item '${item}'...`);
+  console.log('Sending request for quotation to ' + receiver);
 
-  open('https://docs.camunda.org/get-started/quick-start/success');
+  // Define the request body
+  const requestBody = {
+    id: 1,
+    requester: 'HRC',
+    receiver: receiver,
+    description: description,
+    budget: budget, 
+  };
 
-  // Complete the task
-  await taskService.complete(task);
+  // Configure the POST request options
+  const requestOptions = {
+    url: 'http://localhost:9090/quotation_request',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(requestBody),
+  };
+
+  const processVariables = new Variables();
+  const localVariables = new Variables();
+
+  // Send the POST request
+  request(requestOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      console.log('Request for quotation sent successfully');
+      
+      let responseBody;
+
+      try {
+        responseBody = JSON.parse(body);
+        if (responseBody != null) {
+          console.log('Response id:', responseBody.id);
+          console.log('Response Plumber:', responseBody.plumber);
+          console.log('Response quot:', responseBody.quotationPrice);
+          processVariables.set(responseBody.plumber + "_quotation", responseBody.quotationPrice);
+          // Complete the Camunda task
+          taskService.complete(task, processVariables, localVariables);
+        }
+        
+      } catch (error) {
+        console.error('Error parsing response:', error);
+      }
+
+    } else {
+      console.error('Error sending request for quotation:', error);
+      console.error('Response:', body);
+    }
+  });
+});
+
+
+// susbscribe to the topic: 'analyze_offers_plumb'
+client.subscribe('analyze_offers_plumb', async function({ task, taskService }) {
+
+  const list_p = task.variables.get("list_plumbers");
+  const list_quot = [];
+
+
+  list_p.forEach(p => {
+    const quot = task.variables.get(p + "_quotation")
+
+    list_quot.push(quot);
+
+    console.log(p + quot);
+  });
+
+  const processVariables = new Variables();
+  const localVariables = new Variables();
+
+  const minValue = 0;
+  const winner = '';
+  const results_plumbers = [];
+  
+
+  for (let i = 1; i < list_quot.length; i++) {
+    results_plumbers.push([[list_p[i]], 'loser']);
+    if (list_quot[i] < minValue) {
+      minValue = list_quot[i];
+      results_plumbers[i][2] = 'winner';
+    }
+  }
+
+  processVariables.set("results_plumbers", results_plumbers);
+  taskService.complete(task, processVariables, localVariables);
+
+});
+
+client.subscribe('send_res_plumbers', async function({ task, taskService }) {
+  
+  const res_p = task.variables.get("res_p");
+  console.log('Sending result to ' + res_p);
+ 
+  // Define the request body
+  const requestBody = {
+    name_plumber: res_p[1],
+    id: 1,
+    outcome: res_p[2],
+  };
+
+  // Configure the POST request options
+  const requestOptions = {
+    url: 'http://localhost:9090/quotation_request',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(requestBody),
+  };
+
+  const processVariables = new Variables();
+  const localVariables = new Variables();
+
+  // Send the POST request
+  request(requestOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      console.log('Result sent successfully');
+
+      if (res_p[2] == 'winner'){
+        localVariables.set("accepted", true)
+      }
+      else localVariables.set("accepted", false)
+
+      // Complete the Camunda task
+      taskService.complete(task, processVariables, localVariables);
+    } else {
+      console.error('Error sending request for quotation:', error);
+      console.error('Response:', body);
+    }
+  });
 });
--- services/plumber/plumber-server/services/quotationService.js
+++ services/plumber/plumber-server/services/quotationService.js
@@ -1,4 +1,4 @@
-export function addQuotation(req, res, id_request) {
+export function addQuotation(req, res) {
     res.send({
         message: 'This is the mockup controller for addQuotation'
     });
